bdb8d48f5ef123b139404e4ecf6173bd1a51069a
EC2 Default User <ec2-user@ip-172-31-2-171.us-east-2.compute.internal>
2018-06-04 04:58:14 +0000

    user registration

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,4 @@
+                <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                    <li><a href="#">Login</a></li>
+                </ul>
